51089ea265064d75882e3484386bcfa8477f262b
mmmn-s <refocus5959@gmail.com>
2025-08-11 00:11:19 +0900

    Delete assets directory

--- assets/js/common.js
+++
@@ -1,9 +0,0 @@
-var scroll;
-var winH = $(window).height();
-var objTop = $('#intro').offset().top;
-$(window).on('scroll', function(){
-  scroll = $(window).scrollTop();
-  if(scroll >= objTop - winH/2){
-    $('#intro').addClass('.show')
-  }
-});
